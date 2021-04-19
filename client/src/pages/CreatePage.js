8ef545c0fd5df6859288e9fde3bc3a1b13c8ae8d
Kirill Petrov <kllpff@gmail.com>
2021-04-20 00:22:54 +0300

    New commit

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,14 +1,25 @@
-import React, {useState} from 'react'
-import {useHttp} from "../hooks/http.hook";
+import React, {useContext, useEffect, useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import {useHttp} from "../hooks/http.hook"
+import {AuthContext} from "../context/authContext"
 
 export const CreatePage = () => {
+	const history = useHistory()
+	const auth = useContext(AuthContext)
 	const {request} = useHttp()
 	const [link, setLink] = useState('')
+
+	useEffect(() => {
+		window.M.updateTextFields()
+	}, [])
 
 	const pressHandler = async event => {
 		if (event.key === 'Enter') {
 			try {
-				const data = await request('/api/link/generate', 'POST', {from: link})
+				const data = await request('/api/link/generate', 'POST', {from: link}, {
+					Authorization: `Bearer ${auth.token}`
+				})
+				history.push(`/detail/${data.link._id}`)
 				console.log(data)
 			} catch (e) {
 
@@ -21,6 +32,7 @@ export const CreatePage = () => {
 			<div className="col s8 offset-s2" style={{paddingTop: '2rem'}}>
 				<div className="input-field">
 					<input
+						style={{color: 'white'}}
 						placeholder="Paste your link"
 						id="link"
 						type="text"
